3001a65355c708afea4e462e250b67db89223a07
Karuna-Karri <karrikaruna20@gmail.com>
2025-07-03 11:43:24 +0530

    file loaded

--- database.py
+++ database.py
@@ -1,7 +1,19 @@
+ 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-URL_DATABASE='mysql+pymysql://root:Karuna%40%40123@localhost:3306/BlogApplication'
-engine=create_engine(URL_DATABASE)
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-Base=declarative_base()
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+URL_DATABASE = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(URL_DATABASE)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
