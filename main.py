0c5b802111111d6eda2986f71827788daaf5a1fe
Karuna-Karri <karrikaruna20@gmail.com>
2025-07-14 16:44:37 +0530

    added

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from fastapi import FastAPI,HTTPException,Depends,status
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import Request
 from typing import Annotated
 from database import engine,SessionLocal
 from sqlalchemy.orm import Session
 from models import Developer
 import models
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI running fine!"}
+
+
 app=FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+templates = Jinja2Templates(directory="templates")
+
+
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal() 
     try:
         yield db
     finally:
         db.close()
+
 db_dependency=Annotated[Session,Depends(get_db)]    
 
 class DeveloperBase(BaseModel):
@@ -38,5 +54,10 @@ async def get_developer(developer_id: int, db: db_dependency):
     if developer is None:
         raise HTTPException(status_code=404, detail="Developer not found")
     return developer
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 
